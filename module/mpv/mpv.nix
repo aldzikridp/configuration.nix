35165934249aec4439339c6a165c637241266bdf
Eorclus <aldzikridp@gmail.com>
2021-11-06 11:00:39 +0700

    same as before

--- module/mpv/mpv.nix
+++ module/mpv/mpv.nix
@@ -2,7 +2,7 @@
 with import <nixpkgs> { };
 let
   unstable = import
-    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/master.tar.gz)
+    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz)
     # reuse the current configuration
     { config = config.nixpkgs.config; };
 in 
