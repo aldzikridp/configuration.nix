834c86f952ced02db43ee87e8ba856de5414b6b2
Eorclus <aldzikridp@gmail.com>
2022-02-01 17:14:16 +0700

    privoxy adblock

---
+++ module/network/dnsmasq-adblock/privoxy.nix
@@ -0,0 +1,67 @@
+{ pkgs, ... }:
+let
+  curlCA = (builtins.fetchurl {
+    url = "https://curl.se/ca/cacert.pem";
+  });
+
+  certs = pkgs.runCommand "generate-certs"
+    { buildInputs = [ pkgs.openssl ]; }
+    ''
+      mkdir $out
+
+      openssl ecparam -out $out/cakey.pem -name secp384r1 -genkey
+      openssl req -new -x509 -key $out/cakey.pem -sha384 -days 3650 -out $out/cacert.crt -extensions v3_ca -subj "/O=Privoxy CA/CN=Privoxy CA"
+    '';
+
+  adblockList = pkgs.stdenv.mkDerivation {
+    name = "adblockList";
+    list1 = (builtins.fetchurl {
+      url = "https://easylist.to/easylist/easylist.txt";
+    });
+    list2 = (builtins.fetchurl {
+      url = "https://easylist.to/easylist/easyprivacy.txt";
+    });
+    list3 = (builtins.fetchurl {
+      url = "https://easylist.to/easylist/fanboy-annoyance.txt";
+    });
+    list4 = (builtins.fetchurl {
+      url = "https://easylist.to/easylist/fanboy-social.txt";
+    });
+    list5 = (builtins.fetchurl {
+      url = "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt";
+    });
+    list6 = (builtins.fetchurl {
+      url = "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt";
+    });
+    list7 = (builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/ABPindo/indonesianadblockrules/master/subscriptions/abpindo.txt";
+    });
+
+    phases = [ "installPhase" ];
+
+    installPhase = ''
+      ${pkgs.haskellPackages.adblock2privoxy}/bin/adblock2privoxy -p $out/ $list1 $list2 $list3 $list4 $list5 $list6 $list7
+    '';
+  };
+in
+{
+  services.privoxy = {
+    enable = true;
+    inspectHttps = true;
+    settings = {
+      debug = 65536;
+      logfile = "/tmp/privoxy.log";
+      ca-cert-file = "${certs}/cacert.crt";
+      ca-key-file  = "${certs}/cakey.pem";
+      filterfile = [
+       "${adblockList}/ab2p.filter"
+       "${adblockList}/ab2p.system.filter"
+      ];
+      actionsfile = [
+        "${adblockList}/ab2p.action"
+        "${adblockList}/ab2p.system.action"
+      ];
+    };
+  };
+  security.pki.certificateFiles = [ "${certs}/cacert.crt" ];
+}
