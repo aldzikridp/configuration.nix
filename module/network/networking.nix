74b090cf719d7c8db039372fe76a96b0eefaeee2
Al-M4 <aldzikridp@gmail.com>
2022-02-04 06:53:53 +0700

    fix awk expression

--- module/network/networking.nix
+++ module/network/networking.nix
@@ -6,7 +6,7 @@
   # Firewall.
   #networking.firewall.allowedTCPPorts = [ 51413 38692 15441 ];
   #networking.firewall.allowedUDPPorts = [ 51413 38692 15441 ];
-  networking.firewall.allowedTCPPorts = [ 51413 ];
-  networking.firewall.allowedUDPPorts = [ 51413 ];
+  networking.firewall.allowedTCPPorts = [ 51413 53 ];
+  networking.firewall.allowedUDPPorts = [ 51413 53 ];
   networking.firewall.enable = true;
 }
