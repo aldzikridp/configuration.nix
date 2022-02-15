2725b2209ce37d5dd6395e972a8420b3778ac148
Eorclus <aldzikridp@gmail.com>
2022-02-15 16:32:23 +0700

    librewolf already merged

--- module/librewolf/librewolf.nix
+++ module/librewolf/librewolf.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
 with import <nixpkgs> { };
 let
-  squalus_nix = import
-    (builtins.fetchTarball https://github.com/squalus/nixpkgs/archive/refs/heads/librewolf.tar.gz){};
+  unstable = import
+    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/master.tar.gz){};
 in
 {
   environment.systemPackages = with pkgs; [
-    squalus_nix.librewolf
+    unstable.librewolf-wayland
   ];
 }
