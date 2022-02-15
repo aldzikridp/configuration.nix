d0ae9b2f5f889ae0b7d77818af0ea6bb30304f14
Eorclus <aldzikridp@gmail.com>
2022-02-15 16:31:45 +0700

    rename python module

--- module/python/default.nix
+++ module/python/default.nix
@@ -2,12 +2,23 @@
 let
   my-packages = python-packages: with python-packages; [
     pillow
+    python-lsp-server
   ];
   my-python = pkgs.python3.withPackages my-packages;
+
+  jupyterWithBatteries = pkgs.jupyter.override {
+    python3 = pkgs.python3.withPackages (ps: with ps; [
+      numpy
+      scipy
+      matplotlib
+    ]);
+  };
+
 
 in
 {
   environment.systemPackages = with pkgs; [
     my-python
+    jupyterWithBatteries
   ];
 }
