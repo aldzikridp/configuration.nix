45ff8461fbcaf82dd8d62fddfd9ca923ab8aa702
Eorclus <aldzikridp@gmail.com>
2022-02-18 19:13:40 +0700

    use channel instead git directly (compiling takes time)

--- module/librewolf/librewolf.nix
+++ module/librewolf/librewolf.nix
@@ -1,8 +1,7 @@
 { config, pkgs, ... }:
 with import <nixpkgs> { };
 let
-  unstable = import
-    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/master.tar.gz){};
+  unstable = import <nixpkgs-unstable> {};
 in
 {
   environment.systemPackages = with pkgs; [
