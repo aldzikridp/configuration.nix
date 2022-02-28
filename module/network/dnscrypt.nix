5569ede4359ce98204f9be33f61e1587a6988130
Eorclus <aldzikridp@gmail.com>
2022-02-28 20:39:32 +0700

    add more public dnscrypt server

--- module/network/dnscrypt.nix
+++ module/network/dnscrypt.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       listen_addresses = ["127.0.0.1:5300"];
-      ipv6_servers = false;
+      ipv6_servers = true;
       require_dnssec = true;
 
       sources.public-resolvers = {
@@ -17,6 +17,11 @@
         minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
       };
       server_names = [
+        "moulticast-sg-ipv6"
+        "dnswarden-asia-uncensor-dcv6"
+        "sgp-dn53-ipv6"
+        "dnscrypt-sg-blahdns-ipv6"
+        "id-gmail-ipv6"
         "wevpn-singapore"
         "moulticast-sg-ipv4"
         "dnscrypt-sg-blahdns-ipv4"
