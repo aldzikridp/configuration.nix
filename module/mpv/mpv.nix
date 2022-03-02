e81055c9a2987ad9b18b51643d614253397df9d9
Eorclus <aldzikridp@gmail.com>
2022-03-02 19:40:33 +0700

    add mpv script

--- module/mpv/mpv.nix
+++ module/mpv/mpv.nix
@@ -4,7 +4,12 @@ let
   unstable = import <nixpkgs-unstable>
     # reuse the current configuration
     { config = config.nixpkgs.config; };
-  mympv = mpv-with-scripts.override { scripts = [ mpvScripts.thumbnail ]; };
+  mympv = with mpvScripts; mpv-with-scripts.override {
+    scripts = [
+      thumbnail
+      mpv-playlistmanager
+      sponsorblock
+    ]; };
 in
 {
   environment.systemPackages = with pkgs; [
