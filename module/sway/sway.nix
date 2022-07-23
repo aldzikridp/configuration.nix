23bcc43d33b867e41ee304854a9c1798db2cf24f
Eorclus <aldzikridp@gmail.com>
2022-07-24 06:01:39 +0700

    chromium argument

--- module/sway/sway.nix
+++ module/sway/sway.nix
@@ -36,6 +36,20 @@
     unstable.google-chrome
     unstable.onlyoffice-bin
     unstable.tdesktop
-    unstable.ungoogled-chromium
+    (unstable.ungoogled-chromium.override{
+      commandLineArgs = [
+        "--enable-features=UseOzonePlatform"
+        "--ozone-platform=wayland"
+        "--ignore-gpu-blocklist"
+        "--enable-gpu-rasterization"
+        "--enable-zero-copy"
+        "--disable-gpu-driver-bug-workarounds"
+        "--enable-accelerated-video-decode"
+        "--enable-features=VaapiVideoDecoder"
+        "--use-gl=egl"
+        "--show-avatar-button=never"
+        "--hide-sidepanel-button=enable"
+      ];
+    })
   ];
 }
