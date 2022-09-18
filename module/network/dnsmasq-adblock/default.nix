e09be02e7e2864b7a4848b585739bb937cb893b8
Eorclus <aldzikridp@gmail.com>
2022-09-18 19:35:29 +0700

    update hosts block

--- module/network/dnsmasq-adblock/default.nix
+++ module/network/dnsmasq-adblock/default.nix
@@ -4,12 +4,12 @@ let
     name = "adblockhost";
     custom = builtins.toFile"customhost"(builtins.readFile ./customblock);
     host1 = (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/notracking/hosts-blocklists/de01d2b87aad735f58ad54c8ee1eb4c6f0e71a0d/hostnames.txt";
-      sha256 = "046d3iqir9vsycg1xjhm30xbgmd8rya2nywn91dnj7g3kn6j2472";
+      url = "https://raw.githubusercontent.com/notracking/hosts-blocklists/f29f59d5e6de9efd6a237cbeff3df948c7a527bb/hostnames.txt";
+      sha256 = "1n786lvffsvfj2sa7d4643djm2sls4h1lxygcyxk47cp1i7681xm";
     });
     host2 = (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/StevenBlack/hosts/306f1009eab0561c4a80df326fd797c15b1a6430/hosts";
-      sha256 = "0zh5184apb1c6mv8sabfwlg49s6xxapwxq5qid7d48786xggq6wi";
+      url = "https://raw.githubusercontent.com/StevenBlack/hosts/4b00782ca09da629ea343a3c24c46442ce38f770/hosts";
+      sha256 = "16mrf0gd1a3wdw3dnidpb6bsq1fh3y5jqyzgmq6f285pxy9apb70";
     });
     #host3 = (builtins.fetchurl { url = "https://block.energized.pro/assets/sources/filter/abpindo.txt"; });
     #host4 = (builtins.fetchurl { url = "https://block.energized.pro/ultimate/formats/hosts.txt"; });
