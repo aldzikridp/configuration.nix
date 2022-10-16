bf2daf7db38a0d9f5b3a2fcff7613c65603c6445
Eorclus <aldzikridp@gmail.com>
2022-10-16 16:31:48 +0700

    ubridge

--- module/gns3/default.nix
+++ module/gns3/default.nix
@@ -1,12 +1,10 @@
-{ config, pkgs, ... }@inputs:
+{ config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    unstable.gns3-server
-    unstable.gns3-gui
-    unstable.vpcs
-    unstable.ubridge
-    unstable.dynamips
-    unstable.iouyap
+    gns3-server
+    gns3-gui
+    vpcs
+    dynamips
     qemu
   ];
 
@@ -14,15 +12,15 @@
   #virtualisation.libvirtd.enable = true;
   #virtualisation.libvirtd.qemu.runAsRoot = false;
   #programs.dconf.enable = true;
-  #users.users.master-x.extraGroups = [ "libvirtd" ];
+  users.users.master-x.extraGroups = [ "libvirtd" ];
 
   ## Modify .config/GNS3/2.2/gns3_server.config
-  ## then change ubridge_path to /run/current-system/sw/bin/ubridge
+  ## then change ubridge_path to /run/wrappers/bin/ubridge
   security.wrappers.ubridge = {
     source = "${pkgs.ubridge}/bin/ubridge";
-    capabilities = "cap_net_admin+eip cap_net_raw+eip cap_net_broadcast+eip cap_net_bind_service+eip";
-    owner = "nobody";
-    group = "nogroup";
-    #permissions = "u+rx,g+x";
+    capabilities = "cap_net_admin+eip cap_net_raw+eip";
+    owner = "master-x";
+    group = "users";
+    permissions = "u+rx,g+x";
   };
 }
