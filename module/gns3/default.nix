67364e1695aae5a414c362a783c8c62a5b36daef
Eorclus <aldzikridp@gmail.com>
2022-10-16 08:14:55 +0700

    update

--- module/gns3/default.nix
+++ module/gns3/default.nix
@@ -1,17 +1,20 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }@inputs:
 {
   environment.systemPackages = with pkgs; [
-    gns3-server
-    gns3-gui
-    vpcs
-    ubridge
-    dynamips
+    unstable.gns3-server
+    unstable.gns3-gui
+    unstable.vpcs
+    unstable.ubridge
+    unstable.dynamips
+    unstable.iouyap
     qemu
   ];
+
+  ## libvirtd needed for cloud in GNS3
   #virtualisation.libvirtd.enable = true;
-  virtualisation.libvirtd.qemu.runAsRoot = false;
-  programs.dconf.enable = true;
-  users.users.master-x.extraGroups = [ "libvirtd" ];
+  #virtualisation.libvirtd.qemu.runAsRoot = false;
+  #programs.dconf.enable = true;
+  #users.users.master-x.extraGroups = [ "libvirtd" ];
 
   ## Modify .config/GNS3/2.2/gns3_server.config
   ## then change ubridge_path to /run/current-system/sw/bin/ubridge
