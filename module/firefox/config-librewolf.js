a388857455b782324b216063cd4141be059af3d6
aldzikridp <aldzikridp@gmail.com>
2023-12-17 18:25:01 +0700

    rename firefox config

---
+++ module/firefox/config-librewolf.js
@@ -0,0 +1,3 @@
+pref("webgl.disabled", false);
+pref("privacy.clearOnShutdown.history", false);
+pref("privacy.clearOnShutdown.downloads", false);
--- module/firefox/default.nix
+++ module/firefox/default.nix
@@ -4,25 +4,26 @@ let
 myLibrewofl = pkgs.librewolf.override {
   nativeMessagingHosts = [ pkgs.ff2mpv ];
 
-  extraPolicies =
-    import ./policy.nix;
+#  extraPolicies =
+#    import ./policy.nix;
 
   extraPrefs =
-    builtins.readFile ./config.js;
+    builtins.readFile ./config-librewolf.js;
 
 };
 myFirefox = pkgs.firefox.override {
   nativeMessagingHosts = [ pkgs.ff2mpv ];
 
   extraPolicies =
-    import ./policy.nix;
+    import ./policy-firefox.nix;
 
   extraPrefs =
-    builtins.readFile ./config.js;
+    builtins.readFile ./config-firefox.js;
 };
 
 in {
   environment.systemPackages = with pkgs; [
     myFirefox
+    myLibrewofl
   ];
 }
