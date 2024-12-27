e77c0f13ee24c707d5c38905ced9a36da545d8be
aldzikridp <aldzikridp@gmail.com>
2024-12-27 18:01:10 +0700

    foot background color & transparency

--- home/foot.nix
+++ home/foot.nix
@@ -7,6 +7,10 @@
         font = "JetBrainsMono Nerd Font Mono:size=11,Noto Color Emoji:size=11,NotoSans Nerd Font:size=11";
         letter-spacing = 0.5;
       };
+      colors = {
+        background = "000000";
+        alpha = 0.8;
+      };
     };
   };
 }
