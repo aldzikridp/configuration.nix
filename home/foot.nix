b213b6ce24da2b6468fb1d884dd950395728dd8b
aldzikridp <aldzikridp@gmail.com>
2025-01-02 12:35:37 +0700

    foot theme

--- home/foot.nix
+++ home/foot.nix
@@ -7,9 +7,36 @@
         font = "JetBrainsMono Nerd Font Mono:size=11,Noto Color Emoji:size=11,NotoSans Nerd Font:size=11";
         letter-spacing = 0.5;
       };
+      cursor = {
+        #color = "c8d3f5 2d3f76";
+      };
       colors = {
-        background = "000000";
-        alpha = 0.8;
+        #background = "000000";
+        #alpha = 0.8;
+
+        foreground="c8d3f5";
+        background="222436";
+        selection-foreground="c8d3f5";
+        selection-background="2d3f76";
+        urls="4fd6be";
+        regular0="1b1d2b";
+        regular1="ff757f";
+        regular2="c3e88d";
+        regular3="ffc777";
+        regular4="82aaff";
+        regular5="c099ff";
+        regular6="86e1fc";
+        regular7="828bb8";
+        bright0="444a73";
+        bright1="ff757f";
+        bright2="c3e88d";
+        bright3="ffc777";
+        bright4="82aaff";
+        bright5="c099ff";
+        bright6="86e1fc";
+        bright7="c8d3f5";
+        "16"="ff966c";
+        "17"="c53b53";
       };
     };
   };
