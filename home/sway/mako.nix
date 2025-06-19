d5f3b7b83a529e06aa48d86b9a8efef09c5605c5
aldzikridp <aldzikridp@gmail.com>
2025-06-19 20:12:20 +0700

    update

--- home/sway/mako.nix
+++ home/sway/mako.nix
@@ -1,12 +1,14 @@
 {
-  services.mako.settings = {
-   enable = true;
-   max-visible = 3;
-   default-timeout = 15000;
-   anchor = "top-center";
-   font = "Roboto 10";
-   background-color = "#000000";
-   text-color = "#FFFFFF";
-   border-size = 0;
+  services.mako = {
+    enable = true;
+    settings = {
+      max-visible = 3;
+      default-timeout = 15000;
+      anchor = "top-center";
+      font = "Roboto 10";
+      background-color = "#000000";
+      text-color = "#FFFFFF";
+      border-size = 0;
+    };
   };
 }
