a00f1ccf8bce45e32df48009745d9cfeec388115
aldzikridp <aldzikridp@gmail.com>
2026-06-09 17:38:37 +0700

    yazi: add opener

--- home/yazi.nix
+++ home/yazi.nix
@@ -2,5 +2,35 @@
   programs.yazi = {
     enable = true;
     enableBashIntegration = true;
+    settings = {
+      opener = {
+        "video" = [{
+          run = ''mpv --no-terminal "$@" 2&>/dev/null'';
+          orphan = true;
+        }];
+        "image" = [{
+          run   = ''imv "$@"'';
+          orphan = true;
+        }];
+        "pdf" = [{
+          run   = ''zathura "$@"'';
+          orphan = true;
+        }];
+      };
+      open.prepend_rules = [
+        {
+          mime = "image/*";
+          use  = "image";
+        }
+        {
+          mime = "video/*";
+          use  = "video";
+        }
+        {
+          mime = "application/pdf";
+          use = ["zathura"];
+        }
+      ];
+    };
   };
 }
